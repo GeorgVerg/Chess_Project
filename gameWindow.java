3f3983d2f2469a7297518f83f579fee081378692
GeorgVerg <geor.verg@gmail.com>
2024-04-02 19:40:30 +0300

    Fixed .gitignore

---
+++ gameWindow.java
@@ -0,0 +1,12 @@
+package Chess_Project;
+import javax.swing.JFrame;
+
+public class gameWindow 
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
